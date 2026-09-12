de8c3948063d6f8177b857565b35ee2ddcb453d8
auritron <myself.anath@gmail.com>
2026-09-12 18:21:53 +0530

    updated meta.sv for interfacing with CPU MMIO ports

--- src/design/core/meta.sv
+++ src/design/core/meta.sv
@@ -5,6 +5,8 @@ module meta (
     input logic rst_n,
     input logic stop_in,
     input logic valid_instr,
+    // interfacing with meta unit
+    input logic clear,  // syncrhonous clear
     // from hazard unit for telemetry data
     input logic is_stall,
     input logic is_l_use,
@@ -27,17 +29,24 @@ module meta (
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            stop_signal <= 1'b0;
-            instr_count <= '0;
-            stall_count <= '0;
-            l_use_count <= '0;
-            br_flush_count <= '0;
+            stop_signal     <= 1'b0;
+            instr_count     <= '0;
+            stall_count     <= '0;
+            l_use_count     <= '0;
+            br_flush_count  <= '0;
         end else begin
             stop_signal <= stop_in;
-            if (valid_instr)    instr_count <= instr_count + 1'b1;
-            if (is_stall)       stall_count <= stall_count + 1'b1;
-            if (is_l_use)       l_use_count <= l_use_count + 1'b1;
-            if (is_br_flush)    br_flush_count <= br_flush_count + 2'd2;    // add 2 cycles here since flushes void 2 cycles worth of instructions
+            if (clear) begin
+                instr_count     <= '0;
+                stall_count     <= '0;
+                l_use_count     <= '0;
+                br_flush_count  <= '0;
+            end else begin
+                if (valid_instr)    instr_count     <= instr_count + 1'b1;
+                if (is_stall)       stall_count     <= stall_count + 1'b1;
+                if (is_l_use)       l_use_count     <= l_use_count + 1'b1;
+                if (is_br_flush)    br_flush_count  <= br_flush_count + 2'd2;    // add 2 cycles here since flushes void 2 cycles worth of instructions
+            end
         end
     end
 
